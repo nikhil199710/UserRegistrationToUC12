06de09d317e3354e4019963effe7d9907deb2141
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-12 19:49:06 +0530

    [Nikhil] UC5PassRule1AlongMSTest

--- MSTesting/UnitTest1.cs
+++ MSTesting/UnitTest1.cs
@@ -17,5 +17,56 @@ namespace MSTesting
 
             Assert.AreEqual(expected, var);
         }
+
+
+        [TestMethod]
+        public void TestLastName()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Yadav";
+
+            bool var = pattern.validateLastName(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestEmail()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Nikhil";
+
+            bool var = pattern.validateEmail(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestMobile()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "91 7896541238";
+
+            bool var = pattern.validateMobile(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestPass1()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "qwertygrte";
+
+            bool var = pattern.validatePasswordrule1(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+
     }
 }
