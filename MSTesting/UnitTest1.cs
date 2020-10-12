5d7f5e6d70c688b1bbd49feb460fda32af83a7f6
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-12 20:03:41 +0530

    [Nikhil] UC8PassRule4AlongMSTest

--- MSTesting/UnitTest1.cs
+++ MSTesting/UnitTest1.cs
@@ -17,5 +17,91 @@ namespace MSTesting
 
             Assert.AreEqual(expected, var);
         }
+
+
+        [TestMethod]
+        public void TestLastName()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Yadav";
+
+            bool var = pattern.validateLastName(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestEmail()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Nikhil";
+
+            bool var = pattern.validateEmail(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestMobile()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "91 7896541238";
+
+            bool var = pattern.validateMobile(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestPass1()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "qwertygrte";
+
+            bool var = pattern.validatePasswordrule1(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+
+        [TestMethod]
+        public void TestPas2()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Qwertygrte";
+
+            bool var = pattern.validatePasswordWithUppercase(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestPas3()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Qw4ertygrte";
+
+            bool var = pattern.validatePasswordWithNumeric(input);
+
+            Assert.AreEqual(expected, var);
+        }
+
+        [TestMethod]
+        public void TestPas4()
+        {
+            Pattern pattern = new Pattern();
+            bool expected = true;
+            string input = "Qw!4ertygrte";
+
+            bool var = pattern.validatePasswordWithSpecialChar(input);
+
+            Assert.AreEqual(expected, var);
+        }
     }
 }
