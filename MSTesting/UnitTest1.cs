6c910b8e1384142183f01240649733ae4d2270f3
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-12 19:32:58 +0530

    [Nikhil] UC3_EmailAlongMSTest

--- MSTesting/UnitTest1.cs
+++ MSTesting/UnitTest1.cs
@@ -18,6 +18,7 @@ namespace MSTesting
             Assert.AreEqual(expected, var);
         }
 
+        [TestMethod]
         public void TestLastName()
         {
             Pattern pattern = new Pattern();
@@ -29,11 +30,12 @@ namespace MSTesting
             Assert.AreEqual(expected, var);
         }
 
+        [TestMethod]
         public void TestEmail()
         {
             Pattern pattern = new Pattern();
             bool expected = true;
-            string input = "Nikhil";
+            string input = "abc.xyz@bl.co.in";
 
             bool var = pattern.validateEmail(input);
 
