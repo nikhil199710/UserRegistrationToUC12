74bea3363e0f9ad5c8b793008f2fb3afe09732ed
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-10-12 19:41:49 +0530

    [Nikhil] UC4_MobileAlongMSTest

--- User_Registration/Program.cs
+++ User_Registration/Program.cs
@@ -6,10 +6,32 @@ namespace User_Registration
 {
     public class Pattern
     {
-        public static string REGEX_ENTRY = "^[A-Z][a-zA-Z]{2,}$";
+        public static string REGEX_FIRST_NAME = "^[A-Z][a-zA-Z]{2,}$";
         public bool validateFirstName(string firstName)
         {
-            return Regex.IsMatch(firstName, REGEX_ENTRY);
+            return Regex.IsMatch(firstName, REGEX_FIRST_NAME);
+        }
+
+
+
+        public static string REGEX_LAST_NAME = "^[A-Z][a-zA-Z]{2,}$";
+        public bool validateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, REGEX_LAST_NAME);
+        }
+
+
+        public static string REGEX_EMAIL = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
+        public bool validateEmail(string email)
+        {
+            return Regex.IsMatch(email, REGEX_EMAIL);
+        }
+
+
+        public static string REGEX_MOBILE = @"\d{2}\s{1}[0-9]{10}";
+        public bool validateMobile(string mobile)
+        {
+            return Regex.IsMatch(mobile, REGEX_MOBILE);
         }
     }
     class Program
